77f4b28a5f84a8415bccb796693963aa8542fc6d
sb8919 <sb8919@naver.com>
2022-10-26 21:14:34 +0900

    fix_function

--- Travel_plus_labeling.py
+++ Travel_plus_labeling.py
@@ -2,11 +2,7 @@ import pandas as pd
 from pandas import Series, DataFrame
 import numpy as np
 
-<<<<<<< HEAD
 df = pd.read_excel('강진_크롤링.xlsx', engine = "openpyxl")
-=======
-df = pd.read_excel('가평_크롤링.xlsx', engine = "openpyxl")
->>>>>>> e3ef59a7d5070488ebeb2b3a605a93c988cbdc75
 arr = np.array(df)
 test_list = np.delete(arr, 0, axis = 1)
 #test_list = [['장소','본문','태그','img_url'],['장소2','본문2','태그2','img_url2'],['장소3','본문3','태그3','img_url3'],] #입력리스트 ex) 엑셀 데이터
@@ -20,7 +16,6 @@ def labeling(url,place,main,tags):
     print('장소 : '+ place)
     print('본문 : '+ main)
     print('태그 : '+ tags)   
-<<<<<<< HEAD
     sel = input("\n라벨링 해주세요 1.가볼만한곳 2.가족여행 3.우정여행 4.전통 5.체험 6.캠핑 7.관람 8.맛집 9.카페 \n")
     print('') # 분간선
     sel_list = ['1' if l+1 == int(sel) else '0' for l in range(theme)]   
@@ -41,17 +36,3 @@ print(label_output[0])
 label_df = pd.DataFrame(label_output)
 label_df.columns = ['장소','본문','태그','1.가볼만한곳','2.가족여행', '3.우정여행', '4.전통','5.체험', '6.캠핑','7.관람','8.맛집', '9.카페']
 label_df.to_excel('강진_크롤링_v2.xlsx')
-=======
-    sel = input("라벨링 해주세요 1.가볼만한곳 2.가족여행 3.우정여행 4.전통 5.체험 6.캠핑 7.관람 8.맛집 9.카페 \n")
-    sel_list = ['1' if l+1 == int(sel) else '0' for l in range(theme)]   
-    label_output.append([place,main,tags,sel_list])
-    
-for i in range(list_count):
-    labeling(str(test_list[i][0]),str(test_list[i][1]),str(test_list[i][2]),str(test_list[i][3]))
-
-print(label_output)
-
-label_df = pd.DataFrame(label_output)
-label_df.columns = ['장소','본문','태그','가볼만한곳']
-label_df.to_excel('가평_크롤링_v2.xlsx')
->>>>>>> e3ef59a7d5070488ebeb2b3a605a93c988cbdc75
